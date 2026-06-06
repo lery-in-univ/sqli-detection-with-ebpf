8818e4a15be5e7c4252b85ee610d3ee21856458e
Coalery <doralife12@gmail.com>
2026-06-06 16:57:45 +0900

    chore: 프로젝트 스캐폴딩

---
+++ README.md
@@ -0,0 +1,3 @@
+# sqli-detection-with-ebpf
+
+2026-1에 수강한 AI네트워킹 강의의 기말 프로젝트입니다.
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sqli-detection-with-ebpf!")
+
+
+if __name__ == "__main__":
+    main()
